a19761d5a7f03b935924f3963e8c5026f1e66684
amirhoseyn221177 <amirhoseyn221177>
2020-02-17 21:16:57 -0800

    Nav

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Nav from "./Navbar/Navigation"
 
 const Home = (props) => {
     return(
         <div className="Home">
          <h1>Welcome to HQL</h1>
+         <Nav/>
         </div>
     )
 }
