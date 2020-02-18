fe66c598ef90288949d767766b1aa4961efe504f
amirhoseyn221177 <amirhoseyn221177>
2020-02-18 14:27:20 -0800

    nav jadid

--- src/Component/Navbar/Navigation.js
+++ src/Component/Navbar/Navigation.js
@@ -1,14 +1,19 @@
-import React, { Fragment } from "react"
+import React, {useState } from "react"
 import "./Nav.css"
 
 
 const Nav=(props)=>{
+    const [rate,setrate]=useState
+    let checkratehandler=()=>{
+
+    }
     return(
          <header className="Nav">
          <div className="menu" onClick={props.toggle}>
          <div className="bar1"></div>
          <div className="bar2"></div>
          <div className="bar3"></div>
+         <button onClick={}> Check your rate </button>
          </div>
          </header>
 
