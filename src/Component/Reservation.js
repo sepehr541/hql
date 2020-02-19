ffd5bc611c75049b64e558848947ca3e592f5ff7
sepehr541 <sepehr541@gmail.com>
2020-02-18 18:59:54 -0800

    css for reservation

--- src/Component/Reservation.js
+++ src/Component/Reservation.js
@@ -1,17 +1,19 @@
 import React, { Component, Fragment } from 'react'
 import {connect} from "react-redux"
-
+import './Reservation.css'
 class Reservation extends Component {
 
     render() {
         return (
             <Fragment>
-                <div className="Reservation">
+                <div className="Reservation container">
                     <form>
                         <label for="name">Full Name</label>
                         <input type="text" id="name"></input>
                         <label for="phone">Phone number</label>
                         <input type="text" id="phone"></input>
+                        <label for="email">Email</label>
+                        <input type="text" id="email"></input>
                     </form>
                 </div>
             </Fragment>
