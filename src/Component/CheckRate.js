3400030f7efd76a383bb5c01bad8870674320648
sepehr541 <sepehr541@gmail.com>
2020-02-19 01:11:26 -0800

    styled checkrate

--- src/Component/CheckRate.js
+++ src/Component/CheckRate.js
@@ -2,13 +2,13 @@ import React, { Component, Fragment } from 'react'
 import "./Checkrate.css"
 import DateRangePicker from 'react-daterange-picker'
 import 'react-daterange-picker/dist/css/react-calendar.css'
-import {connect} from "react-redux"
-import {gettingdate} from "../Actions/action"
+import { connect } from "react-redux"
+import { gettingdate } from "../Actions/action"
 import { withRouter } from 'react-router-dom'
 class CheckRate extends Component {
 
-    state={
-        visible:true
+    state = {
+        visible: true
     }
 
     handleChange = (event) => {
@@ -19,44 +19,38 @@ class CheckRate extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState(prev=>({
-            visible:!prev.visible
+        this.setState(prev => ({
+            visible: !prev.visible
         })
-         
+
         )
         this.props.history.push("/resv")
 
     }
-    
+
     onSelect = (dates) => {
         console.log(dates);
-        let nums=document.getElementById("nums").value
-        this.props.send_dates(dates.start._d,dates.end._d,nums)
+        let nums = document.getElementById("nums").value
+        this.props.send_dates(dates.start._d, dates.end._d, nums)
 
     }
 
-    
-
-
     render() {
-        let show=null
-        if(this.state.visible){
-            show=(
+        let show = null
+        if (this.state.visible) {
+            show = (
                 <Fragment>
-                        <div >
-                            <form className="check" onSubmit={this.handleSubmit}>
-                                <input name="number" id="nums"   style={{marginTop:"50px"}}  type="text" placeholder="Number of Adults" />
-                            </form>
-                            <form className="check2">
-                            <input name="date" type="text" placeholder="Check-in and check-out"  value={this.props.checkin}></input>
-    
-                            </form>
-    
-                                <DateRangePicker onSelect={this.onSelect} />
-    
-                                <button onClick={this.handleSubmit}>Check</button>
-                        </div>
-                    </Fragment>)
+                    <div >
+                        <form className="check" onSubmit={this.handleSubmit}>
+                        <ul>
+                            <li><input name="number" id="nums" type="text" placeholder="Number of Adults" /></li>
+                            <li id="date-field"><input name="date" type="text" placeholder="Check-in and check-out" value={this.props.checkin}></input></li>
+                            <li id="date-picker"><DateRangePicker onSelect={this.onSelect} /></li>
+                            <li><button onClick={this.handleSubmit}>Check</button></li>
+                        </ul>
+                        </form>
+                    </div>
+                </Fragment>)
         }
 
         return (
@@ -65,17 +59,17 @@ class CheckRate extends Component {
     }
 }
 
-const maptostate=state=>{
-    return{
-        checkin:state.dates.check_in
+const maptostate = state => {
+    return {
+        checkin: state.dates.check_in
 
     }
 }
-const maptoprops=dispatch=>{
-    return{
-        send_dates:(start,end,people)=>dispatch(gettingdate(start,end,people))
+const maptoprops = dispatch => {
+    return {
+        send_dates: (start, end, people) => dispatch(gettingdate(start, end, people))
     }
 }
 
 
-export default connect(maptostate,maptoprops) (withRouter(CheckRate));
+export default connect(maptostate, maptoprops)(withRouter(CheckRate));
