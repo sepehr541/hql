2d484f2762aaeacd91b74e902b8e7f66f445ea4f
amirhoseyn221177 <amirhoseyn221177>
2020-02-19 17:26:59 -0800

    video

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -7,13 +7,12 @@ const Home = (props) => {
             <title>Vancouver </title>
             </head>
             {/* <iframe width="1000" height="678" src="https://www.youtube.com/embed/WbfjRoQVZSw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
-            <body>
             <video width="540" height="310" controls>
-                <source src="~/Desktop/hql/vancouver.mp4" type="video/mp4"/>
-                <source src="~/Desktop/hql/vancouver.mp4" type="video/ogg"/>
+                <source src="~/Desktop/vancouver.mp4" type="video/mp4"/>
+                <source src="~/Desktop/vancouver.mp4" type="video/ogg"/>
+
 
              </video>
-            </body>
    
      
         </div>
