2610dd9cc9736d2c60973ab3999e0a9dbca88749
sepehr541 <sepehr541@gmail.com>
2020-02-19 16:18:11 -0800

    backdrop working

--- src/Component/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./Backdrop.css"
-
-const backdrop=(props)=>(
-    props.show ? <div className="back" onClick={props.click}></div>:null
-)
-
-export default backdrop
