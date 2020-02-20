ada2eced62dad5a8ce8590ef37dc4e4489336df5
amirhoseyn221177 <amirhoseyn221177>
2020-02-19 22:07:07 -0800

    helllo

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,11 +4,11 @@ import Video from "../vancouver.mp4"
 
 const Home = (props) => {
     return(
-        <div classN ame="Home">
+        <div className="Home">
         
             {/* <iframe width="1000" height="678" src="https://www.youtube.com/embed/WbfjRoQVZSw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
            
-            <video className="video" loop  width="1000" height="500" controls>
+            <video preload="auto" muted className="video" loop  width="1000" height="500" controls>
                 <source src={Video} type="video/mp4"/>
                 
              </video>
