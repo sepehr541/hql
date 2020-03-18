d42a7ae44f2950ef938a8a366260db81ef177746
amirhoseyn221177 <amirhoseyn221177>
2020-03-18 00:00:08 -0700

    changed the video player

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import "./video.css"
 import Video from "../../src/vancouver.mp4"
+import van from './PosterPic.jpg'
 
 const Home = (props) => {
+
+
+
+  
   return (
-    <div>
-      <video src={Video} className="video" loop controls muted autoplay>
-        <source type="video/mp4" />
-      </video>
-    </div>
+
+    <Fragment>
+      <div className="vidcontain">
+        <div className="vid"></div>
+  <video playsinline="playsinline" poster={van} controls   autoplay muted="muted" loop="loop">
+    <source src={Video} type="video/mp4"/>
+  </video>
+
+</div>
+<div>
+<p style={{position:"relative", color:"red" , marginTop:"1200px"}}>dafadfsdsadasdddddddddddddddsadasdasdasdasdasdasdasdasdasdasdasdasdaad</p>
+
+</div>
+
+  
+
+
+    </Fragment>
+    
+
   )
 }
 
