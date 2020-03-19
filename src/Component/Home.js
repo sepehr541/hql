a317478b3f016173b8774d9ac0f3a77d3301404b
sepehr541 <sepehr541@gmail.com>
2020-03-18 17:18:11 -0700

    hhhh

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,36 +1,33 @@
-import React, { Fragment ,useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import "./video.css"
 import Video from "../../src/vancouver.mp4"
 import van from './PosterPic.jpg'
 
 const Home = (props) => {
-  
 
-  
+
+
   return (
 
-      <Fragment style={{position:"relative"}}>
-        <div className="vidcontain">
-    <video playsinline="playsinline"  controls poster={van} autoPlay   muted="muted" loop="loop">
-      <source src={Video} type="video/mp4"/>
-    </video>
+    <Fragment style={{ position: "relative" }}>
+      <div className="vidcontain">
+        <video playsinline="playsinline" controls poster={van} autoPlay muted="muted" loop="loop">
+          <source src={Video} type="video/mp4" />
+        </video>
 
-  </div>
-  <div>
-  
-    <h1 style={{position:"relative", fontSize:"40px", color:"white" , marginTop:"28%", marginLeft:"42%"}}> Welcome to HQL
-    <h1 style={{marginLeft: "90px", marginTop:"-20px"}}> ____</h1>
-    </h1>
-  
+      </div>
+      <div>
+
+        <h1 style={{ position: "relative", fontSize: "40px", color: "white", marginTop: "28%", marginLeft: "42%" }}> Welcome to HQL
+    <h1 style={{ marginLeft: "90px", marginTop: "-20px" }}> ____</h1>
+        </h1>
+      </div>
 
 
-</div>
-
-  
 
 
     </Fragment>
-    
+
 
   )
 }
