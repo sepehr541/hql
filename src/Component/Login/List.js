47103e7ee5770cde7f43c83a7ed8a04bd2215282
sepehr541 <sepehr541@gmail.com>
2020-03-31 02:34:02 -0700

    partial fix 2

--- src/Component/Login/List.js
+++ src/Component/Login/List.js
@@ -1,18 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 const List = (props) => {
-    const items = useRef([]);
-    useEffect(() => {
-        console.log(props.data);
-        items.current = props.data.map(d => <li className="collection-item">{d}</li>)
-        
-    }, [items, props.data])
-
     return (
         <div className='col s3'>
             <ul class="collection with-header">
                 <li class="collection-header">{props.title}</li>
-                {items.current}
+                {props.data.map(d => <li className="collection-item">{d}</li>)}
             </ul>
         </div>
     )
