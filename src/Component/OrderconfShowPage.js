55fa9ce6ee35b1730b5b8738eb9b2a9c5734ca6c
sepehr541 <sepehr541@gmail.com>
2020-03-30 22:15:56 -0700

    a

---
+++ src/Component/OrderconfShowPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from './OrderConfirmation/orderconfirmation'
+
+const Order=(props)=>{
+
+    return(
+        <Search/>
+    )
+}
+
+
+export default Order;
